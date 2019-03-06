fe9c33b4e7b83fe909fdcaddddbd7f7aba3925cc
Matthew Davidson <mad1512@optusnet.com.au>
2019-03-06 23:34:11 +1100

    Worked on pod setup and generic build phase run script for post pod installation

---
+++ LingoExample/Pods/Target Support Files/Lingo/Lingo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Lingo : NSObject
+@end
+@implementation PodsDummy_Lingo
+@end
---
+++ LingoExample/Pods/Target Support Files/Pods-LingoExampleUITests/Pods-LingoExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LingoExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LingoExampleUITests
+@end
